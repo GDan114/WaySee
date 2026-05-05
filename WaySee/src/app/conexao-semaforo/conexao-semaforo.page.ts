f46a6ee69884fe31353986a2f32ad75488672875
EficieAndre <eficieandre@gmail.com>
2026-05-05 11:31:41 -0300

    5/5

--- WaySee/src/app/conexao-semaforo/conexao-semaforo.page.ts
+++ WaySee/src/app/conexao-semaforo/conexao-semaforo.page.ts
@@ -2,21 +2,43 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon } from '@ionic/angular/standalone';
-import { RouterModule } from '@angular/router';
-
+import { RouterModule, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-conexao-semaforo',
   templateUrl: './conexao-semaforo.page.html',
   styleUrls: ['./conexao-semaforo.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon, RouterModule]
+  imports: [CommonModule, FormsModule, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon, RouterModule]
 })
 export class ConexaoSemaforoPage implements OnInit {
 
-  constructor() { }
+  private urlDoEsp = 'http://192.168.4.1/status'; 
+
+  constructor(
+    private router: Router, 
+    private http: HttpClient
+  ) { }
 
   ngOnInit() {
+    this.tentarConectarSemaforo();
   }
 
+  tentarConectarSemaforo() {
+    console.log('Buscando semáforo na rede...');
+
+    this.http.get(this.urlDoEsp, { observe: 'response' }).subscribe({
+      next: (resposta) => {
+        if (resposta.status === 200) {
+          console.log('Semáforo encontrado e respondendo!');
+          this.router.navigate(['/exito-conexao']); 
+        }
+      },
+      error: (erro) => {
+        console.error('Semáforo não encontrado. Erro:', erro);
+        this.router.navigate(['/erro-conexao']); 
+      }
+    });
+  }
 }
--- WaySee/src/app/erro-conexao/erro-conexao.page.ts
+++ WaySee/src/app/erro-conexao/erro-conexao.page.ts
@@ -4,16 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon } from '@ionic/angular/standalone';
 import { RouterModule } from '@angular/router';
 
+// Importações necessárias para o ícone funcionar no modo Standalone
+import { addIcons } from 'ionicons';
+import { homeOutline } from 'ionicons/icons';
+
 @Component({
   selector: 'app-erro-conexao',
   templateUrl: './erro-conexao.page.html',
   styleUrls: ['./erro-conexao.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon, RouterModule]
+  imports: [CommonModule, FormsModule, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon, RouterModule]
 })
 export class ErroConexaoPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    // Registra o ícone da casinha para poder ser usado no HTML
+    addIcons({ homeOutline });
+  }
 
   ngOnInit() {
   }

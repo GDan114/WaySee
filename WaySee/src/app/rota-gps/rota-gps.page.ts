ef7be225deaf1ed956f990c30ae9f4f9776968b1
GDan114 <133902539+GDan114@users.noreply.github.com>
2026-05-05 11:26:58 -0300

    05-05-Implementção rota
    rota funcionando de forma simples

--- WaySee/src/app/rota-gps/rota-gps.page.ts
+++ WaySee/src/app/rota-gps/rota-gps.page.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon, IonInput, IonItem, IonList } from '@ionic/angular/standalone';
 import { RouterModule } from '@angular/router';
+import { Geolocation } from '@capacitor/geolocation';
+import * as L from 'leaflet';
 
 
 @Component({
@@ -10,13 +12,38 @@ import { RouterModule } from '@angular/router';
   templateUrl: './rota-gps.page.html',
   styleUrls: ['./rota-gps.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon, RouterModule],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon,  IonInput, IonItem, IonList, RouterModule, FormsModule],
 })
+
 export class RotaGpsPage implements OnInit {
+  endereco: string = '';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  async abrirRota() {
+  if (!this.endereco) return;
+
+  const destino = encodeURIComponent(this.endereco);
+
+  try {
+    const pos = await Geolocation.getCurrentPosition();
+
+    const lat = pos.coords.latitude;
+    const lng = pos.coords.longitude;
+
+    const url = `https://www.google.com/maps/dir/?api=1&origin=${lat},${lng}&destination=${destino}`;
+
+    window.open(url, '_system');
+
+  } catch (err) {
+    console.log('Erro ao pegar localização', err);
+
+    // fallback (caso usuário negue permissão)
+    const url = `https://www.google.com/maps/dir/?api=1&destination=${destino}`;
+    window.open(url, '_system');
+  }
+}
 }
